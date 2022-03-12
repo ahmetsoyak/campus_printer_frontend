585caae8290be81eb844a5b9b086a6661c77ef91
Ahmet Soyak <ahmetsoyak35@hotmail.com>
2022-03-12 10:37:23 +0300

    Add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -12,11 +12,17 @@ const routes = [
     path: '/',
     name: 'home',
     component: page('Home')
+  },
+  {
+    path: '*',
+    name: '404',
+    component: page('Error/404.vue')
   }
 ];
 
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 });
-
 export default router;
