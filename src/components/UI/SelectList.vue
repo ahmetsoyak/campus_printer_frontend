52fc6fbc00f9494eae57e4635b4ed77f87c3021c
Ahmet Soyak <ahmetsoyak35@hotmail.com>
2022-03-15 20:59:45 +0300

    Add custom select list component

---
+++ src/components/UI/SelectList.vue
@@ -0,0 +1,32 @@
+<template>
+  <select class="form-select" label="label" v-model="selected">
+    <option v-for="option in options" :value="option.key" :key="option.key">
+      {{ option.label }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'Select-List',
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    value: {
+      type: [String, Array, Object]
+    }
+  },
+  computed: {
+    selected: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      }
+    }
+  }
+};
+</script>
