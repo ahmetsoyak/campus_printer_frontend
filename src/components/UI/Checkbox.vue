9dc5fef5b923c4eeced7a86cc89c9ccdb5d4bd22
Ahmet Soyak <ahmetsoyak35@gmail.com>
2022-08-13 16:30:41 +0300

    Add Checkbox UI component

---
+++ src/components/UI/Checkbox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <input
+      type="checkbox"
+      class="mr-2"
+      :name="name"
+      :id="name"
+      v-model="checkboxValue"
+    />
+    <label :for="name">
+      <slot name="label"> {{ label }} </slot>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Checkbox-UI',
+  props: {
+    value: {
+      type: Boolean
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    checkboxValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      }
+    }
+  }
+};
+</script>
